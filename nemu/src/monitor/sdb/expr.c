88c62f4a9415437967802ed4bfcbb05990caf2ae
tracer-ics2025 <tracer@njuics.org>
2026-03-24 21:51:37 +0800

    >  compile NEMU 231220000 张三 Linux MSI 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:51:37 up 11:26,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -76,6 +76,79 @@ typedef struct token {
 static Token tokens[32] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
+static bool check_parentheses(int p, int q){
+  if (tokens[p].type != '(' || tokens[q].type != ')'){
+    return false;
+  }
+  int cnt = 0;
+  for (int i = p; i <= q; i++){
+    if (tokens[i].type == '('){
+      cnt++;
+    } else if (tokens[i].type == ')'){
+      cnt--;
+    }
+    if (cnt == 0 && i < q){
+      return false;
+    }
+  }
+  return cnt == 0;
+}
+
+static int find_main_op(int p, int q){
+  int op = -1;
+  int in_parentheses = 0;
+  int op_priority = 100;
+
+  for (int i = p; i <= q; i++){
+    if (tokens[i].type == '('){
+      in_parentheses++;
+      continue;
+    } else if (tokens[i].type == ')'){
+      in_parentheses--;
+      continue;
+    }
+    if (in_parentheses > 0){
+      continue;
+    }
+    int current_priority = 0;
+    if (tokens[i].type == '+' || tokens[i].type == '-'){
+      current_priority = 1;
+    } else if (tokens[i].type == '*' || tokens[i].type == '/'){
+      current_priority = 2;
+    } else {
+      continue;
+    }
+    if (current_priority <= op_priority){
+      op_priority = current_priority;
+      op = i;
+    }
+  }
+  return op;
+}
+
+word_t eval(int p, int q){
+  if (p > q) {
+    assert(0);
+  } else if (p == q){
+    int num;
+    sscanf(tokens[p].str, "%d", &num);
+    return num;
+  } else if (check_parentheses(p, q) == true){
+    return eval(p + 1, q - 1);
+  } else {
+    int op = find_main_op(p, q);
+    word_t val1 = eval(p, op - 1);
+    word_t val2 = eval(op + 1, q);
+    switch (tokens[op].type){
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;
+      case '*': return val1 * val2;
+      case '/': return val1 / val2;
+      default: assert(0);
+    }
+  }
+}
+
 static bool make_token(char *e) {
   int position = 0;
   int i;
@@ -145,9 +218,6 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+  *success = true;
+  return eval(0, nr_token - 1);
 }
