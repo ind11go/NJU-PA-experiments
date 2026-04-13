3684674584a3bfc55a44477b4f98a2a96fb3ad53
tracer-ics2025 <tracer@njuics.org>
2026-04-13 19:35:28 +0800

    >  compile NEMU 231220000 张三 Linux MSI 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:35:28 up 23 min,  1 user,  load average: 0.00, 0.01, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -7,14 +7,30 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t screen_info = inl(VGACTL_ADDR);
+  uint32_t w = screen_info >> 16;
+  uint32_t h = screen_info & 0xffff;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = w, .height = h,
+    .vmemsz = w * h * sizeof(uint32_t)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR; 
+  uint32_t *pixels = (uint32_t *)ctl->pixels;    
+  
+  int x = ctl->x, y = ctl->y;
+  int w = ctl->w, h = ctl->h;
+  
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+
+  for (int j = 0; j < h; j++) {
+    for (int i = 0; i < w; i++) {
+      fb[(y + j) * screen_w + (x + i)] = pixels[j * w + i];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
