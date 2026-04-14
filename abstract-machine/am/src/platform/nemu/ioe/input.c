ccdea387680acad469655e777ae3c6b901a63b7d
tracer-ics2025 <tracer@njuics.org>
2026-04-14 11:25:32 +0800

    >  compile NEMU 231220000 张三 Linux MSI 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:25:32 up 43 min,  1 user,  load average: 0.00, 0.04, 0.05

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,6 +1,5 @@
 #include <am.h>
 #include <nemu.h>
-#include <stdio.h>
 
 #define KEYDOWN_MASK 0x8000
 
@@ -8,7 +7,4 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t scancode = inl(KBD_ADDR);
   kbd->keydown = (scancode & KEYDOWN_MASK) ? true : false;
   kbd->keycode = scancode & ~KEYDOWN_MASK;
-  if (kbd->keycode != AM_KEY_NONE) {
-      printf("Bus Signal: 0x%08x | Down: %d | KeyCode: %d\n", scancode, kbd->keydown, kbd->keycode);
-  }
 }
