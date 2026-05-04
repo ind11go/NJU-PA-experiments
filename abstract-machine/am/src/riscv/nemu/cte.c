7a03ad4b67b31f058798890de255dedf167c6d1e
tracer-ics2025 <tracer@njuics.org>
2026-05-04 22:53:56 +0800

    >  compile NEMU 231220000 张三 Linux MSI 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:53:56 up  9:51,  1 user,  load average: 1.06, 0.89, 0.56

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -14,6 +14,9 @@ Context* __am_irq_handle(Context *c) {
 
     c = user_handler(ev, c);
     assert(c != NULL);
+
+    // Advance mepc past the ecall instruction
+    c->mepc += 4;
   }
 
   return c;
