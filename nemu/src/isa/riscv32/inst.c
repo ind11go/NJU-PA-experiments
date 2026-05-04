cca197c8c615272c0fcd228dc5a39fb4492c8acc
tracer-ics2025 <tracer@njuics.org>
2026-05-04 22:45:40 +0800

    >  compile NEMU 231220000 张三 Linux MSI 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:45:40 up  9:43,  1 user,  load average: 0.31, 0.25, 0.18

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -115,7 +115,7 @@ static int decode_exec(Decode *s) {
 
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(0xb, s->snpc));
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(0xb, s->pc));
   INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N,
     s->dnpc = cpu.mepc;
     uint32_t mpie = (cpu.mstatus >> 7) & 1;
