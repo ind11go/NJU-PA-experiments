779a6e4f2f1f853868c96c8846ec3a0b9b099ddb
tracer-ics2025 <tracer@njuics.org>
2026-05-05 12:10:10 +0800

    >  compile NEMU 231220000 张三 Linux MSI 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:10:10 up 52 min,  1 user,  load average: 0.00, 0.01, 0.00

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -1,11 +1,11 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-/* Uncomment these macros to enable corresponding functionality. */
-//#define HAS_CTE
-//#define HAS_VME
-//#define MULTIPROGRAM
-//#define TIME_SHARING
+
+#define HAS_CTE
+#define HAS_VME
+#define MULTIPROGRAM
+#define TIME_SHARING
 
 #include <am.h>
 #include <klib.h>
